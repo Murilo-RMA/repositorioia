061b27ba633a0e4e76a376e1818d810c34963774
Murilo_RMA <murilo.santos7@ba.estudante.senai.br>
2024-11-18 15:52:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("login-form")?.addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Login realizado com sucesso!");
+  });
+  
+  document.getElementById("recover-form")?.addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Instruções para recuperação de senha enviadas.");
+  });
